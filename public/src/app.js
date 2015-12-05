1691627ae2c3673e0e089c812f25b31b00d7c548
Sergey Smirnov <s@tigr.bz>
2015-12-05 15:44:21 +0300

    add controller.

--- public/src/app.js
+++ public/src/app.js
@@ -1,4 +1,9 @@
-angular.module('ContactsApp', [])
-.run(function ($rootScope) {
-	$rootScope.message = "Hallo Angular!";
-});
+angular.module('ContactsApp', ['ngRoute'])
+	.config(function ($routeProvider, $locationProvider) {
+		$routeProvider
+				.when('/contacts', {
+					controller: 'ListController',
+					templateUrl: 'views/list.html'
+				});
+		$locationProvider.html5Mode(true);
+	});
